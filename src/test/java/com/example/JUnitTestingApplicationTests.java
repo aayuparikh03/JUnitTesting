7fec8da56f93025b886c07d75d85dd5a3cb8ca2f
Aayu Parikh <aryanparikh03@gmail.com>
2025-01-30 12:17:08 +0530

    Exception related testing is implemented

--- src/test/java/com/example/JUnitTestingApplicationTests.java
+++ src/test/java/com/example/JUnitTestingApplicationTests.java
@@ -2,9 +2,21 @@ package com.example;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+class Calculator{
+	public int divide(int a,int b)
+	{
+		if(b==0)
+		{
+			throw new ArithmeticException("Can't divide by zero");
+		}
+		return a/b;
+	}
+
+
+}
 
 @SpringBootTest
 class JUnitTestingApplicationTests {
@@ -13,27 +25,11 @@ class JUnitTestingApplicationTests {
 
 	@Test
 	void contextLoads() {
-		Person expected=new Person("Aayu",21);
-		Person actual=new Person("Aayu",25);
-		assertEquals(expected,actual,"No objects are equals");
+		Calculator calculator=new Calculator();
+//
+		assertThrows(ArithmeticException.class,(()->calculator.divide(3,0)),"Exception is not occured");
 
 	}
 
 }
-class Person{
-	private String name;
-	private int age;
 
-	public Person(String name, int age) {
-		this.name = name;
-		this.age = age;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if(this==obj) return true;
-		if(obj==null || getClass()!=obj.getClass()) return false;
-		Person p=(Person) obj;
-		return age==p.age && name.equals(p.name);
-	}
-}
