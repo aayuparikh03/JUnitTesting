da8ae10b0ffddc38c8d0b68076891231db7cb7a4
Aayu Parikh <aryanparikh03@gmail.com>
2025-01-30 14:07:45 +0530

    Value score annotation is implemented

--- src/test/java/com/example/JUnitTestingApplicationTests.java
+++ src/test/java/com/example/JUnitTestingApplicationTests.java
@@ -1,31 +1,19 @@
 package com.example;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
-
-
-import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class PerformanceTest{
-	void sampleMethod() throws InterruptedException
-	{
-		Thread.sleep(300);
-	}
-
-
-
-}
-
 @SpringBootTest
 class JUnitTestingApplicationTests {
-
-
-
-	@Test
-	void contextLoads() {
-		PerformanceTest performanceTest=new PerformanceTest();
-		assertTimeout(Duration.ofMillis(500),()->performanceTest.sampleMethod(),"Method takes too long");
+	boolean isEven(int num){
+		return num%2==0;
+	}
+	@org.junit.jupiter.params.ParameterizedTest
+	@ValueSource(ints={2,4,6,8})
+	void testIsEven(int num){
+		assertTrue(isEven(num),"Number should be even only");
 	}
 
 }
