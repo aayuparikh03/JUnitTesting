d3596f230680ffb46125518602c9759105b244b9
Aayu Parikh <aryanparikh03@gmail.com>
2025-01-30 10:46:46 +0530

    Comparing of array test is being implemented

--- src/test/java/com/example/JUnitTestingApplicationTests.java
+++ src/test/java/com/example/JUnitTestingApplicationTests.java
@@ -1,17 +1,20 @@
 package com.example;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class JUnitTestingApplicationTests {
 
+
 	@Test
 	void contextLoads() {
 		int[] expected={1,2,3,4};
-		int [] actual={1,2,3,4};
-		assertEq
-
+		int[] actual={1,2,3,4};
+		assertTrue(Arrays.equals(expected,actual),"Arrays are not equal!");
 
 	}
 
